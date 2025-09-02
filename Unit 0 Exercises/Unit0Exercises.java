b5906456c0e8ece5ae203681afdbabbd31986ea3
ayang4-byte <ayang4@hwemail.com>
2025-09-02 15:02:59 -0700

    Update Unit0Exercises.java

--- Unit 0 Exercises/Unit0Exercises.java
+++ Unit 0 Exercises/Unit0Exercises.java
@@ -12,7 +12,7 @@ public class Unit0Exercises {
         // sleepIn(false, true) -> true
 
         // testing diff21() method
-        System.out.println(obj.diff21(19)); // ouput: 2
+        System.out.println(obj.diff21(19)); // output: 2
         // diff21(10) -> 11
         // diff21(21) -> 0
 
@@ -24,35 +24,27 @@ public class Unit0Exercises {
     }
 
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (vacation == true){ 
+        if (vacation == true) { 
             return true;
-        }
-        if (weekday == false) {
+        } else if (weekday == false) {
             return true;
-        }
-        else {
-            return false;
-        }
+        } else {
+            return false; }
     }
 
     public int diff21(int n) {
         if (n < 21) {
-            return (21-n);
-        }
-        else {
-            return 2 * (n-21);
-        }
+            return (21 - n);
+        } else {
+            return 2 * (n - 21); }
     }
 
     public double flooringCalculator(int sqft, double pricePerSqft) {
         if (sqft <= 0) {
             return 0;
-        }
-        if (pricePerSqft <= 0) {
+        } else if (pricePerSqft <= 0) {
             return 0;
-        }
-        else {
-            return sqft * pricePerSqft;
-        }
+        } else {
+            return sqft * pricePerSqft; }
     }
 }
