b173d47585e299fd4e2f214595c27c65694133b3
ayang4-byte <ayang4@hwemail.com>
2025-09-16 13:30:58 -0700

    Update StudentTester.java

--- Unit 0 Student/StudentTester.java
+++ Unit 0 Student/StudentTester.java
@@ -1,9 +1,8 @@
 public class StudentTester {
     public static void main(String[] args) {
-        Student Johnny = new Student("Johnny", 10);
-        Student Amy = new Student("Amy", 11);
-        System.out.println(Johnny.generateId());
-        System.out.println(Johnny.equals(Amy));
-        
+        Student sophomore = new Student("Johnny", 10);
+        Student junior = new Student("Amy", 11);
+        System.out.println(sophomore.generateId());
+        System.out.println(sophomore.equals(junior));
     }
 }
