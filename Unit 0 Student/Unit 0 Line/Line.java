ec01ba3b6bc1e47a4c0f2c78ca8cbd7b6c87160d
ayang4-byte <ayang4@hwemail.com>
2025-09-22 13:54:33 -0700

    Added Stretch Challenge

--- Unit 0 Student/Unit 0 Line/Line.java
+++ Unit 0 Student/Unit 0 Line/Line.java
@@ -9,6 +9,38 @@ public class Line {
         this.c = c;
     }
 
+    public Line() {
+        this.a = generateRandomParameters();
+        this.b = generateRandomParameters();
+        this.c = generateRandomParameters();
+    }
+
+    public int generateRandomParameters() {
+        int num1 = 1;
+        double num2 = Math.random();
+        if (num2 == 0) {
+            num2 = num2 + 0.1;
+        }
+        int finalNum = (int) (num1 / num2);
+        int generator = (int) (Math.random());
+        if (generator == 0) {
+            finalNum = finalNum * -1;
+        }
+        return finalNum;
+    }
+
+    public String isParallel(Line other) {
+        if (calculateSlope() == other.calculateSlope()) {
+            return "Parallel";
+        } else {
+            double xCoord = (((double) other.c / other.b) - ((double) this.c / this.b)) / 
+                            (((double) other.a / other.b) - ((double) this.a / this.b));
+            double yCoord = ((c * -1) - (a * xCoord)) / b;
+            return "(" + xCoord + ", " + yCoord + ")";
+        } 
+
+    }
+
     public int getA() {
         return a;
     }
@@ -46,7 +78,7 @@ public class Line {
     }
 
     public String toString() {
-        return ("" + a + "x " + b + "y " + c + " = 0");
+        return ("" + a + "x " + " + " + b + "y " + " + " + c + " = 0");
     }
 
     public boolean equals(Line otherLine) {
--- Unit 0 Student/Unit 0 Line/LineTester.java
+++ Unit 0 Student/Unit 0 Line/LineTester.java
@@ -1,9 +1,12 @@
 public class LineTester {
     public static void main(String[] args) {
-        Line alpha = new Line(5, 6, 7);
-        Line beta = new Line(6, 7, 8);
+        Line alpha = new Line(1, 1, 0);
+        Line beta = new Line(1, -1, 0);
+        Line gamma = new Line();
         System.out.println(alpha.calculateSlope());
         System.out.println(alpha.equals(beta));
+        System.out.println(gamma.toString());
+        System.out.println(beta.isParallel(alpha));
     }
     
 }
