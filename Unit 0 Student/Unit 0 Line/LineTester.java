cc9b3b3a4652cf463ab78f301c5563df78e93302
ayang4-byte <ayang4@hwemail.com>
2025-09-22 13:22:50 -0700

    Update LineTester.java

--- Unit 0 Student/Unit 0 Line/LineTester.java
+++ Unit 0 Student/Unit 0 Line/LineTester.java
@@ -1,9 +1,9 @@
 public class LineTester {
     public static void main(String[] args) {
-        Line Alpha = new Line(5, 6, 7);
-        Line Beta = new Line(6, 7, 8);
-        System.out.println(Alpha.calculateSlope());
-        System.out.println(Alpha.equals(Beta));
+        Line alpha = new Line(5, 6, 7);
+        Line beta = new Line(6, 7, 8);
+        System.out.println(alpha.calculateSlope());
+        System.out.println(alpha.equals(beta));
     }
     
 }
